45f928c1aa911177c6a1e288326b64c9d98d751a
SankalpThorat02 <sankalpt2225@gmail.com>
2026-04-28 16:13:51 +0530

    feat: Implement Member Repository

--- src/main/java/com/sankalp/library_api/repositories/MemberRepository.java
+++ src/main/java/com/sankalp/library_api/repositories/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.sankalp.library_api.repositories;
 
-public class MemberRepository {
+import com.sankalp.library_api.models.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
